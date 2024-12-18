5445e8b96df59898eb370eb843458de89a5c1c4e
AksharaaRaghu <akshararaghu2004@gmail.com>
2024-12-18 12:00:53 +0530

    addition completed

--- app/src/main/java/com/example/neewcalculator/MainActivity.java
+++ app/src/main/java/com/example/neewcalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.neewcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1,b2,b3,b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        b1=(Button) findViewById(R.id.addbutt);
+        b2=(Button) findViewById(R.id.subbutt);
+        b3=(Button) findViewById(R.id.mulbutt);
+        b4=(Button) findViewById(R.id.divbutt);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob=new Intent(getApplicationContext(),AdditionActivity.class);
+                startActivity(ob);
+            }
+        });
 
     }
 }
